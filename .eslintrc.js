0d15e0641a968dd8710b341b62ccd32d43996945
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-14 22:28:35 +0800

    解决parser wrong unexpected token < 和 parse wrong keyword import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
 module.exports = {
-    "rules": {
-        "no-console": "off",
+    env: {
+        mocha: true
+    },
+    root: true,
+    parserOptions: {
+        sourceType: 'module'
+    },
+    extends: ["plugin:vue/base"],
+    // required to lint *.vue files
+    plugins: [
+
+    ],
+    // add your custom rules here
+    'rules': {
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-console': "off",
     }
 }
