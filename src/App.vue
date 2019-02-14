12ca88b5ab817703c2048bb4ee457cffcb8de1b9
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-15 02:35:38 +0800

    完成button-group组件

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,30 @@
     <x-button>你好</x-button>
     <x-button icon="download">下载</x-button>
     <x-button icon="thumb-up" icon-position="right">点赞</x-button>
-    <x-button loading="loading" icon="loading" >加载中</x-button>
+    <x-button loading>加载中</x-button>
+
+    <br>
+    <x-button-group class="buttonGroup">
+      <x-button icon="left">向左</x-button>
+      <x-button icon="right" icon-position="right">向右</x-button>
+    </x-button-group>
   </div>
 </template>
 
 <script>
-import Button from './components/button'
+import Button from './components/button/button.vue'
+import ButtonGroup from './components/button/button-group.vue'
 export default {
   name: 'app',
   components: {
-    "x-button":Button
+    "x-button":Button,
+    "x-button-group":ButtonGroup
   }
 }
 </script>
+
+<style>
+  .buttonGroup{
+    margin-top: 10px;
+  }
+</style>
---
+++ src/components/button/button-group.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="x-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "XButtonGroup",
+        mounted() {
+            for (let node of this.$el.children) {
+                let name = node.nodeName.toLowerCase()
+                if (name !== 'button') {
+                    console.warn('x-button-group里的子元素应该是x-button，但你写的是${name}')
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "../../css/basic.scss";
+    .x-button-group{
+        display: inline-flex;
+        vertical-align: center;
+        > .x-button{
+            border-radius: 0;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+            &:first-child{
+                border-top-left-radius: $border-radius;
+                border-bottom-left-radius: $border-radius;
+            }
+            &:last-child{
+                border-top-right-radius: $border-radius;
+                border-bottom-right-radius: $border-radius;
+            }
+            &:hover{
+                position: relative;
+                z-index: -1;
+            }
+        }
+    }
+</style>
