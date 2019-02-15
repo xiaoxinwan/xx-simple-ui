38651580d6885b4675dfffafe210127210f0177c
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-16 01:29:29 +0800

    完成无gutter的col和row

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <x-input value="张三" disabled></x-input>
-    <x-input value="李四"></x-input>
-    <x-input value="李新喜" readonly></x-input>
+    <x-row>
+      <x-col>1</x-col>
+      <x-col>2</x-col>
+    </x-row>
+    <x-row>
+      <x-col>1</x-col>
+      <x-col>2</x-col>
+      <x-col>3</x-col>
+    </x-row>
+    <x-row>
+      <x-col span="2">1</x-col>
+      <x-col span="22">2</x-col>
+    </x-row>
+    <!--<x-input value="张三" disabled></x-input>-->
+    <!--<x-input value="李四"></x-input>-->
+    <!--<x-input value="李新喜" readonly></x-input>-->
 
-    <div style="margin-top: 20px">
-      <x-input value="李一一一" error="姓名不能多于4个字"></x-input>
-    </div>
+    <!--<div style="margin-top: 20px">-->
+      <!--<x-input value="李一一一" error="姓名不能多于4个字"></x-input>-->
+    <!--</div>-->
     <!---->
     <!--<x-button>你好</x-button>-->
     <!--<x-button icon="download">下载</x-button>-->
@@ -25,12 +38,16 @@
 import Button from './components/button/button.vue'
 import ButtonGroup from './components/button/button-group.vue'
 import Input from './components/input/input.vue'
+import Row from './components/grid/row'
+import Col from './components/grid/col'
 export default {
   name: 'app',
   components: {
     "x-button":Button,
     "x-button-group":ButtonGroup,
-    'x-input':Input
+    'x-input':Input,
+    'x-row':Row,
+    'x-col':Col
   }
 }
 </script>
---
+++ src/components/grid/col.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "XCol",
+        props: {
+            span: {
+                type: [Number, String]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        background: #aaa;
+        width: 50%;
+        border: 1px solid #f1453d;
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24)*100%;
+            }
+        }
+    }
+</style>
