b75a374dd701b73cc3d0e2b657a569829f938f61
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-15 16:51:15 +0800

    input组件支持双向绑定

--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="wrapper" :class="{error}">
-        <input :value="value" type="text" :disabled="disabled" :readonly="readonly">
+        <input :value="value" type="text" :disabled="disabled" :readonly="readonly"
+               @change="$emit('change', $event.target.value)"
+               @input="$emit('input', $event.target.value)"
+               @focus="$emit('focus', $event.target.value)"
+               @blur="$emit('blur', $event.target.value)">
         <template v-if="error">
             <icon name="error" class="icon-error"></icon>
             <span class="error-message">{{error}}</span>
@@ -29,7 +33,6 @@
             error: {
                 type: String
             }
-
         }
     }
 </script>
@@ -38,17 +41,50 @@
     @import "../../css/basic";
 
     .wrapper {
-        font-size: $font-size;display: inline-flex;align-items: center;
-        > :not(:last-child){margin-right: .3em;}
-        > input {height: 32px;border: 1px solid $border-color;
-            border-radius: $border-radius;padding: 0 8px;
-            font-size: inherit;margin-left: 1em;
-            &:hover {border: 1px solid $border-color-hover;}
-            &:focus {box-shadow: inset 0 1px 3px $box-shadow-color;outline: none;}
-            &[disabled], &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed;}
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+
+        > :not(:last-child) {
+            margin-right: .3em;
         }
-        &.error { > input {border-color: $red;} }
-        .icon-error{fill: $red;}
-        .error-message{color: $red;}
+
+        > input {
+            height: 32px;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            padding: 0 8px;
+            font-size: inherit;
+            margin-left: 1em;
+
+            &:hover {
+                border: 1px solid $border-color-hover;
+            }
+
+            &:focus {
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+
+            &[disabled], &[readonly] {
+                border-color: #bbb;
+                color: #bbb;
+                cursor: not-allowed;
+            }
+        }
+
+        &.error {
+            > input {
+                border-color: $red;
+            }
+        }
+
+        .icon-error {
+            fill: $red;
+        }
+
+        .error-message {
+            color: $red;
+        }
     }
 </style>
