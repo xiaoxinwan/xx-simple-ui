9e70ea9df25327dda6bde4c1d750dafca7138114
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-17 00:02:38 +0800

    完成layout的上中下布局的4种变式

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,39 @@
 <template>
     <div id="app">
-        <div style="border: 1px solid black">
-            <x-row gutter="20">
-                <x-col span="12"
-                       :ipad="{span: 8}"
-                       :narrow-pc="{span: 4}"
-                       :pc="{span: 2}"
-                       :wide-pc="{span: 1}"
-                >1
-                </x-col>
-                <x-col span="12"
-                       :ipad="{span: 16}"
-                       :narrow-pc="{span: 20}"
-                       :pc="{span: 22}"
-                       :wide-pc="{span: 23}"
-                >2</x-col>
-            </x-row>
-        </div>
-        <!--<x-row gutter="20">-->
-        <!--<x-col>1</x-col>-->
-        <!--<x-col>2</x-col>-->
-        <!--<x-col>3</x-col>-->
-        <!--</x-row>-->
-        <!--<x-row>-->
-        <!--<x-col span="2" offset="2">1</x-col>-->
-        <!--<x-col span="20">2</x-col>-->
-        <!--</x-row>-->
-        <!--<x-row>-->
-        <!--<x-col span="3" offset="2">3</x-col>-->
-        <!--<x-col span="5" offset="4">5</x-col>-->
-        <!--<x-col span="10">10</x-col>-->
-        <!--</x-row>-->
-        <!--<x-row gutter="20">-->
-        <!--<x-col>1</x-col>-->
-        <!--<x-col>2</x-col>-->
-        <!--<x-col>3</x-col>-->
-        <!--<x-col>4</x-col>-->
-        <!--<x-col>5</x-col>-->
-        <!--<x-col>6</x-col>-->
-        <!--<x-col>7</x-col>-->
-        <!--<x-col>8</x-col>-->
-        <!--<x-col>9</x-col>-->
-        <!--<x-col>10</x-col>-->
-        <!--<x-col>11</x-col>-->
-        <!--<x-col>12</x-col>-->
-        <!--<x-col>13</x-col>-->
-        <!--<x-col>14</x-col>-->
-        <!--<x-col>15</x-col>-->
-        <!--<x-col>16</x-col>-->
-        <!--<x-col>17</x-col>-->
-        <!--<x-col>18</x-col>-->
-        <!--<x-col>19</x-col>-->
-        <!--<x-col>20</x-col>-->
-        <!--<x-col>21</x-col>-->
-        <!--<x-col>22</x-col>-->
-        <!--<x-col>23</x-col>-->
-        <!--<x-col>24</x-col>-->
-        <!--</x-row>-->
-        <!--</div>-->
-        <!--<x-input value="张三" disabled></x-input>-->
-        <!--<x-input value="李四"></x-input>-->
-        <!--<x-input value="李新喜" readonly></x-input>-->
+        <!-- 侧+上中下布局 -->
+        <x-layout style="height: 100vh;">
+            <x-sider class="demo demo-sider">sider</x-sider>
+            <x-layout>
+                <x-header class="demo demo-header">header</x-header>
+                <x-content class="demo demo-content">content</x-content>
+                <x-footer class="demo demo-footer">footer</x-footer>
+            </x-layout>
+        </x-layout>
+        <!--上（中+侧）下布局-->
+        <!--<x-layout>-->
+            <!--<x-header class="demo demo-header">header</x-header>-->
+            <!--<x-layout direction="row">-->
+                <!--<x-content class="demo demo-content">content</x-content>-->
+                <!--<x-sider class="demo demo-sider">sider</x-sider>-->
+            <!--</x-layout>-->
+            <!--<x-footer class="demo demo-footer">footer</x-footer>-->
+        <!--</x-layout>-->
+        <!--上（侧+中）下布局-->
+        <!--<x-layout>-->
+            <!--<x-header class="demo demo-header">header</x-header>-->
+            <!--<x-layout direction="row">-->
+                <!--<x-sider class="demo demo-sider">sider</x-sider>-->
+                <!--<x-content class="demo demo-content">content</x-content>-->
+            <!--</x-layout>-->
+            <!--<x-footer class="demo demo-footer">footer</x-footer>-->
+        <!--</x-layout>-->
+        <!--上中下布局-->
+        <!--<x-layout style="height: 100vh;">-->
+            <!--<x-header class="demo demo-header">header</x-header>-->
+            <!--<x-content class="demo demo-content">content</x-content>-->
+            <!--<x-footer class="demo demo-footer">footer</x-footer>-->
+        <!--</x-layout>-->
 
-        <!--<div style="margin-top: 20px">-->
-        <!--<x-input value="李一一一" error="姓名不能多于4个字"></x-input>-->
-        <!--</div>-->
-        <!---->
-        <!--<x-button>你好</x-button>-->
-        <!--<x-button icon="download">下载</x-button>-->
-        <!--<x-button icon="thumb-up" icon-position="right">点赞</x-button>-->
-        <!--<x-button loading>加载中</x-button>-->
-
-        <!--<br>-->
-        <!--<x-button-group class="buttonGroup">-->
-        <!--<x-button icon="left">向左</x-button>-->
-        <!--<x-button icon="right" icon-position="right">向右</x-button>-->
-        <!--</x-button-group>-->
     </div>
 </template>
 
@@ -85,50 +43,37 @@
     import Input from './components/input/input.vue'
     import Row from './components/grid/row'
     import Col from './components/grid/col'
+    import Layout from './components/layout'
 
     export default {
         name: 'app',
         components: {
-            "x-button": Button,
-            "x-button-group": ButtonGroup,
-            'x-input': Input,
-            'x-row': Row,
-            'x-col': Col
+            'x-layout': Layout,
+            'x-header': Layout.Header,
+            'x-content': Layout.Content,
+            'x-sider': Layout.Sider,
+            'x-footer': Layout.Footer
         }
     }
 </script>
 
 <style lang="scss">
-    /*.buttonGroup{*/
-    /*margin-top: 10px;*/
-    /*}*/
-    .topBar {
-
-    }
-
-    .demoBox {
-        background: #e8eaec;
-        min-height: 50px;
-
-        img {
-
+    .demo{
+        min-height: 100px;
+        text-align: center;
+        &.demo-header{
+            background: #CCD3E0;
+        }
+        &.demo-content{
+            background: #646D78;
+        }
+        &.demo-sider{
+            background: #26272E;
+            color: #ffffff;
+        }
+        &.demo-footer{
+            background: #ABB4BF;
         }
     }
 
-    .demo {
-        min-height: 50px;
-        background: #515a6e;
-        color: #ffffff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-right: 20px;
-    }
-
-    .logoWrapper {
-        padding: 10px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
 </style>
--- src/components/layout/layout.vue
+++ src/components/layout/layout.vue
@@ -1,17 +1,36 @@
 <template>
-    <div class="layout">
+    <div class="layout" :class="layoutClass">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "XLayout"
+        name: "XLayout",
+        data() {
+            return {
+                layoutClass: {
+                    hasSider: false
+                }
+            }
+        },
+        mounted() {
+            this.$children.forEach((vm) => {
+                if (vm.$options.name === 'XSider') {
+                    this.layoutClass.hasSider = true
+                }
+            })
+        }
     }
 </script>
 
 <style lang="scss" scoped>
     .layout{
-        
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        &.hasSider{
+            flex-direction: row;
+        }
     }
 </style>
