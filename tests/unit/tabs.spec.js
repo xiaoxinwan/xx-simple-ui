82a7264e47d55f218628bd403f01a240ebb0c472
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-18 23:27:11 +0800

    添加tabs.spec.js 测试

---
+++ tests/unit/tabs.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import Tabs from '@/components/tabs/tabs'
+
+describe('Tabs.vue', () => {
+    it('存在', ()=> {
+        expect(Tabs).to.exist
+    })
+})
