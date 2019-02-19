06148fcdbd98d3d874f2925f0cc47269bc78a4b5
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-19 18:15:38 +0800

    完成点击触发的popover

--- src/components/popover/popover.vue
+++ src/components/popover/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+    <div class="popover" @click="handleClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
         <div ref="triggerWrapper">
@@ -18,22 +18,43 @@
             }
         },
         methods: {
-            xxx() {
-                this.visible = !this.visible
-                if (this.visible === true) {
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-                        let eventHandler = () => {
-                            this.visible = false
-                            document.removeEventListener('click', eventHandler)
-                        }
-                        document.addEventListener('click', eventHandler)
-                    })
+            handleClick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
+                    if (this.visible === true) {
+                        this.handleClose()
+                    } else {
+                        this.handleOpen()
+                    }
                 }
+            },
+            locateContent() {
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            eventHandler(e) {
+                if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+                    return
+                }
+                if (this.$refs.contentWrapper && (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))) {
+                    return
+                }
+                this.handleClose()
+            },
+            handleOpen() {
+                this.visible = true
+                this.$nextTick(() => {
+                    this.locateContent()
+                    document.addEventListener('click', this.eventHandler)
+                })
+            },
+            handleClose() {
+                this.visible = false
+                document.removeEventListener('click', this.eventHandler)
+
             }
+
         }
     }
 </script>
