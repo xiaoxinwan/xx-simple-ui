2c7103e83f78a2cbda49931a5fc4d36565ec8b4e
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-20 18:31:15 +0800

    移动index.js位置

---
+++ src/index.js
@@ -0,0 +1,28 @@
+export {default as Button} from './components/button/button'
+export {default as ButtonGroup} from './components/button/button-group'
+export {default as Collapse} from './components/collapse/collapse'
+export {default as CollapseItem} from './components/collapse/collapse-item'
+export {default as Col} from './components/grid/col'
+export {default as Row} from './components/grid/row'
+export {default as Icon} from './components/icon'
+export {default as Input} from './components/input'
+export {default as Layout} from './components/layout/layout'
+export {default as Content} from './components/layout/content'
+export {default as Footer} from './components/layout/footer'
+export {default as Header} from './components/layout/header'
+export {default as Sider} from './components/layout/sider'
+export {default as Popover} from './components/popover'
+export {default as Tabs} from './components/tabs'
+export {default as TabsBody} from './components/tabs/tabs-body'
+export {default as TabsHead} from './components/tabs/tabs-head'
+export {default as TabsItem} from './components/tabs/tabs-item'
+export {default as TabsPane} from './components/tabs/tabs-pane'
+export {default as Toast} from './components/toast'
+export {default as Plugin} from './components/toast/toastPlugin'
+
+
+
+
+
+
+
