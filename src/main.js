4e927f897752ea8945946393227a8e8e66fb5f42
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-20 18:24:37 +0800

    新建src的入口文件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ToastPlugin from './components/toast/toastPlugin'
 
 Vue.config.productionTip = false
-Vue.use(ToastPlugin)
 
 new Vue({
   render: h => h(App),
