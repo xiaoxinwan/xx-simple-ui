680f18958eec9ee580dd02f0bcc887cf28cdd7a6
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-21 22:58:56 +0800

    更新index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,21 @@
-import XButton from './components/button/button'
-import XButtonGroup from './components/button/button-group'
-import XCollapse from './components/collapse/collapse'
-import XCollapseItem from './components/collapse/collapse-item'
-import XCol from './components/grid/col'
-import XRow from './components/grid/row'
-import XIcon from './components/icon'
-import XInput from './components/input'
-import XLayout from './components/layout/layout'
-import XContent from './components/layout/content'
-import XFooter from './components/layout/footer'
-import XHeader from './components/layout/header'
-import XSider from './components/layout/sider'
-import XPopover from './components/popover'
-import XTabs from './components/tabs'
-import XTabsBody from './components/tabs/tabs-body'
-import XTabsHead from './components/tabs/tabs-head'
-import XTabsItem from './components/tabs/tabs-item'
-import XTabsPane from './components/tabs/tabs-pane'
-import XToast from './components/toast'
-import Plugin from './components/toast/toastPlugin'
-
-const components = [
-    XButton,
-    XButtonGroup,
-    XCollapse,
-    XCollapseItem,
-    XCol,
-    XRow,
-    XIcon,
-    XInput,
-    XLayout,
-    XContent,
-    XFooter,
-    XHeader,
-    XSider,
-    XPopover,
-    XTabs,
-    XTabsBody,
-    XTabsHead,
-    XTabsItem,
-    XTabsPane
-]
-const install = Vue => {
-    components.forEach(component => {
-        component.install(Vue)
-    })
-    Vue.use(XToast)
-    Vue.use(Plugin)
-}
-
-
-export { XToast, Plugin }
-export default { install, XToast, Plugin }
-
-
+export {default as Button} from './components/button/button'
+export {default as ButtonGroup} from './components/button/button-group'
+export {default as CollapseItem} from './components/collapse/collapse-item'
+export {default as Collapse} from './components/collapse/collapse'
+export {default as Col} from './components/grid/col'
+export {default as Row} from './components/grid/row'
+export {default as Content} from './components/layout/content'
+export {default as Footer} from './components/layout/footer'
+export {default as Header} from './components/layout/header'
+export {default as Layout} from './components/layout/layout'
+export {default as Sider} from './components/layout/sider'
+export {default as Tabs} from './components/tabs/tabs'
+export {default as TabsBody} from './components/tabs/tabs-body'
+export {default as TabsHead} from './components/tabs/tabs-head'
+export {default as TabsItem} from './components/tabs/tabs-item'
+export {default as TabsPane} from './components/tabs/tabs-pane'
+export {default as Icon} from './components/icon/icon'
+export {default as Input} from './components/input/input'
+export {default as Plugin} from './components/toast/toastPlugin'
+export {default as Popover} from './components/popover/popover'
+export {default as Toast} from './components/toast/toast'
