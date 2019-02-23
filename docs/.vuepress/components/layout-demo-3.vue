2725b896c626731f31e1ceee99da32a5b12daf6d
xiaoxinwan <lixinxi1010@gmail.com>
2019-02-23 12:20:34 +0800

    完成layout文档

---
+++ docs/.vuepress/components/layout-demo-3.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <x-layout>
+            <x-sider class="siderDemo">Sider</x-sider>
+            <x-layout>
+                <x-header class="headerDemo">Header</x-header>
+                <x-content class="contentDemo">Content</x-content>
+                <x-footer class="footerDemo">Footer</x-footer>
+            </x-layout>
+        </x-layout>
+    </div>
+</template>
+
+<script>
+    import Layout from '../../../src/components/layout/layout'
+    import Header from '../../../src/components/layout/header'
+    import Sider from '../../../src/components/layout/sider'
+    import Content from '../../../src/components/layout/content'
+    import Footer from '../../../src/components/layout/footer'
+    export default {
+        name: "layout-demo-3",
+        components: {
+            'x-layout': Layout,
+            'x-header': Header,
+            'x-sider': Sider,
+            'x-content': Content,
+            'x-footer': Footer
+        }
+    }
+</script>
+
+<style scoped lang='scss'>
+    .headerDemo{
+        background: #CCD3E0;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .siderDemo{
+        background: #26272E;
+        color: #fff;
+        width: 80px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .contentDemo{
+        background: #646D78;
+        height: 80px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .footerDemo{
+        background: #ABB4BF;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
--- docs/components/layout.md
+++ docs/components/layout.md
@@ -1,5 +1,7 @@
 # Layout
 
+`x-header`、`x-content`、`x-sider`和`x-footer`必须是`x-layout`的子组件。
+
 ### 普通上中下布局
 <br>
 <layout-demo-1></layout-demo-1>
@@ -13,4 +15,34 @@
 </x-layout>
 ```
 
+### 上中下布局（含侧边栏）
+<br>
+<layout-demo-2></layout-demo-2>
 
+### 使用
+```
+<x-layout>
+    <x-header class="headerDemo">Header</x-header>
+    <x-layout>
+        <x-sider class="siderDemo">Sider</x-sider>
+        <x-content class="contentDemo">Content</x-content>
+    </x-layout>
+    <x-footer class="footerDemo">Footer</x-footer>
+</x-layout>
+```
+
+### 侧边栏 + 上中下布局
+<br>
+<layout-demo-3></layout-demo-3>
+
+### 使用
+```
+<x-layout>
+    <x-sider class="siderDemo">Sider</x-sider>
+    <x-layout>
+        <x-header class="headerDemo">Header</x-header>
+        <x-content class="contentDemo">Content</x-content>
+        <x-footer class="footerDemo">Footer</x-footer>
+    </x-layout>
+</x-layout>
+```  
